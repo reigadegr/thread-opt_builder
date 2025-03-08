6e1d31aa734756f42ce6c1096e6be6b7378fe4af
reigadegr <272268642@qq.com>
2025-03-08 19:19:48 +0800

    fix: 低级错误导致pgo未生效

--- pgo.sh
+++ pgo.sh
@@ -2,8 +2,10 @@
 # rustup component add llvm-tools-preview
 pgo_path=$(realpath ./target/pgo-data)
 
-RUSTFLAGS="-C default-linker-libraries \
+export RUSTFLAGS="-C default-linker-libraries \
  -C profile-use=$pgo_path/merged.profdata \
  -C link-args=-fomit-frame-pointer \
 -C link-args=-Wl,--as-needed,--icf=all,-z,relro,--pack-dyn-relocs=android+relr,-x,-s,--strip-all,-z,now
-" python3 ./make.py build --release --nightly -v
+"
+echo $RUSTFLAGS
+python3 ./make.py build --release --nightly -v
