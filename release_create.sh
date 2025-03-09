af1a8d43a5bb2d52288565bd8754eead1bbae311
reigadegr <272268642@qq.com>
2025-03-09 17:55:13 +0800

    misc

--- release_create.sh
+++ release_create.sh
@@ -2,6 +2,6 @@
 tag_name="pgo"
 tag_description="pgo v2.0.1"
 
-git tag -a "pgo" -m "pgo v2.0.1"
-git push origin "pgo"
-gh release create $tag_name --title tag_description --notes-file module/changelog.md "./merged.profdata"
+# git tag -a "pgo" -m "pgo v2.0.1"
+# git push origin "pgo"
+gh release create "$tag_name" --title "$tag_description" --notes-file module/changelog.md "./merged.profdata"
