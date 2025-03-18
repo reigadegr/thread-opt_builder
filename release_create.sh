c020bf4b8bc00cb6e5fbcb22acb91cefd4c0a7f5
reigadegr <272268642@qq.com>
2025-03-18 20:53:08 +0800

    增加非pgo构建

--- release_create.sh
+++ release_create.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-tag_name="pgo"
-tag_description="pgo v2.0.1"
+tag_name="v2.1.0"
+tag_description="Release v2.1.0"
 
-# git tag -a "pgo" -m "pgo v2.0.1"
-# git push origin "pgo"
-gh release create "$tag_name" --title "$tag_description" --notes-file module/changelog.md "./merged.profdata"
+git tag -a "v2.1.0" -m "Release v2.1.0"
+git push origin "v2.1.0"
+gh release create "$tag_name" --title "$tag_description" --notes-file "module/changelog.md" "./thread-opt_release.zip"
